5ab0c51d1c4b70a8c9c6ed17b9d944b59df4f974
Ionică Bizău <bizauionica@gmail.com>
2016-02-23 16:19:22 +0200

    Added the example

--- example/index.js
+++ example/index.js
@@ -2,4 +2,29 @@
 
 const classMethods = require("../lib");
 
-console.log(classMethods());
+class Person {
+    constructor (name) {
+        this.setName(name);
+    }
+    setName (name) {
+        this.name = name;
+    }
+    getName (name) {
+        return this.name;
+    }
+}
+
+class Worker extends Person {
+    constructor (name, job) {
+        super(name);
+        this.setJob(job);
+    }
+    setJob (job) {
+        this.job = job;
+    }
+    getJob (job) {
+        return this.job;
+    }
+}
+
+console.log(classMethods(Worker));
